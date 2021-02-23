9a8a197181a54e64841d00dd8e179c36513bac95
Sam Blake <34226571+chiefwithbeef44@users.noreply.github.com>
2021-02-23 13:46:02 -0800

    removed ctre tuning

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,17 +4,12 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
-import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
-
-import frc.CTRELib.Constant;
 import frc.robot.commands.DriveCommand;
 import frc.robot.subsystems.SwerveDriveSystem;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -30,8 +25,6 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
   XboxController _joy = new XboxController(0);
   SwerveDriveSystem sds = new SwerveDriveSystem();
   DriveCommand driveCommand = new DriveCommand(sds, _joy);
-  PowerDistributionPanel pdp = new PowerDistributionPanel(17);
-  TalonFX _talon = sds.m_frontLeft.mAngleMotor;
 
   private Command m_autonomousCommand;
 
@@ -103,7 +96,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
    @Override
    public void teleopPeriodic() 
    {
-     commonLoop();
+
    }
 
    @Override
@@ -114,129 +107,8 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
    /** This function is called periodically during test mode. */
    @Override
-   public void testPeriodic() {}
+   public void testPeriodic() 
+   {
 
-    /** Used to create string thoughout loop */
-  StringBuilder _sb = new StringBuilder();
-  int _loops = 0;
-
-    /** Track button state for single press event */
-  boolean _lastButton1 = false;
-
-  /** Save the target position to servo to */
-  double targetPositionRotations;
-
-public void ctreTuningRobotInit() {
-  /* Factory Default all hardware to prevent unexpected behaviour */
-  _talon.configFactoryDefault();
-  
-  /* Config the sensor used for Primary PID and sensor direction */
-      _talon.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 
-                                          Constant.kPIDLoopIdx,
-                                  Constant.kTimeoutMs);
-
-  /* Ensure sensor is positive when output is positive */
-  _talon.setSensorPhase(Constant.kSensorPhase);
-
-  /**
-   * Set based on what direction you want forward/positive to be.
-   * This does not affect sensor phase. 
-   */ 
-  _talon.setInverted(Constant.kMotorInvert);
-  /*
-   * Talon FX does not need sensor phase set for its integrated sensor
-   * This is because it will always be correct if the selected feedback device is integrated sensor (default value)
-   * and the user calls getSelectedSensor* to get the sensor's position/velocity.
-   * 
-   * https://phoenix-documentation.readthedocs.io/en/latest/ch14_MCSensor.html#sensor-phase
-   */
-      // _talon.setSensorPhase(true);
-
-  /* Config the peak and nominal outputs, 12V means full */
-  _talon.configNominalOutputForward(0, Constant.kTimeoutMs);
-  _talon.configNominalOutputReverse(0, Constant.kTimeoutMs);
-  _talon.configPeakOutputForward(1, Constant.kTimeoutMs);
-  _talon.configPeakOutputReverse(-1, Constant.kTimeoutMs);
-
-  /**
-   * Config the allowable closed-loop error, Closed-Loop output will be
-   * neutral within this range. See Table in Section 17.2.1 for native
-   * units per rotation.
-   */
-  _talon.configAllowableClosedloopError(0, Constant.kPIDLoopIdx, Constant.kTimeoutMs);
-
-  /* Config Position Closed Loop gains in slot0, tsypically kF stays zero. */
-  _talon.config_kF(Constant.kPIDLoopIdx, Constant.kGains.kF, Constant.kTimeoutMs);
-  _talon.config_kP(Constant.kPIDLoopIdx, Constant.kGains.kP, Constant.kTimeoutMs);
-  _talon.config_kI(Constant.kPIDLoopIdx, Constant.kGains.kI, Constant.kTimeoutMs);
-  _talon.config_kD(Constant.kPIDLoopIdx, Constant.kGains.kD, Constant.kTimeoutMs);
-  }
-  
-void commonLoop() {
-  /* Gamepad processing */
-  double leftYstick = _joy.getY(Hand.kLeft);
-  boolean button1 = _joy.getXButton();	// X-But;ton
-
-  /* Get Talon's current output percentage */
-  double motorOutput = _talon.getMotorOutputVoltage()/pdp.getVoltage();
-
-  /* Deadband gamepad */
-  if (Math.abs(leftYstick) < 0.10) {
-    /* Within 10% of zero */
-    leftYstick = 0;
-  }
-
-  /* Prepare line to print */
-  _sb.append("\tout:");
-  /* Cast to int to remove decimal places */
-  _sb.append((int) (motorOutput * 100));
-  _sb.append("%");	// Percent
-
-  _sb.append("\tpos:");
-  _sb.append(_talon.getSelectedSensorPosition(0));
-  _sb.append("u"); 	// Native units
-
-  /**
-   * When button 1 is pressed, perform Position Closed Loop to selected position,
-   * indicated by Joystick position x10, [-10, 10] rotations
-   */
-  if (!_lastButton1 && button1) {
-    /* Position Closed Loop */
-
-    /* 10 Rotations * 2048 u/rev in either direction */
-    targetPositionRotations = leftYstick * 10.0 * 2048;
-    _talon.set(TalonFXControlMode.Position, targetPositionRotations);
-  }
-
-  /* If Talon is in position closed-loop, print some more info */
-  if (_talon.getControlMode() == TalonFXControlMode.Position.toControlMode()) {
-    /* ppend more signals to print when in speed mode. */
-    _sb.append("\terr:");
-    _sb.append(_talon.getClosedLoopError(0));
-    _sb.append("u");	// Native Units
-
-    _sb.append("\ttrg:");
-    _sb.append(targetPositionRotations);
-    _sb.append("u");	/// Native Units
-  }
-
-  /**
-   * Print every ten loops, printing too much too fast is generally bad
-   * for performance.
-   */
-  if (++_loops >= 10) {
-    _loops = 0;
-    System.out.println(_sb.toString());
-  }
-
-  /* Reset built string for next loop */
-  _sb.setLength(0);
-  
-  /* Save button state for on press detect */
-  _lastButton1 = button1;
-  }
-  
-/**
- * This function is called periodically during operator control
- */
-}
+   }
+ }
